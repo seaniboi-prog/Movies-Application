6c00d1bf506d4307cc098a014a14bf368723e20a
seanfarrugia <sean.farrugia.19@um.edu.mt>
2023-03-07 23:58:28 +0100

    Removed unnecessary dependencies

--- movie-gallery/src/main/java/com/betsson/movie/api/MovieGalleryApplication.java
+++ movie-gallery/src/main/java/com/betsson/movie/api/MovieGalleryApplication.java
@@ -13,13 +13,4 @@ public class MovieGalleryApplication {
 		SpringApplication.run(MovieGalleryApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
-//			}
-//		};
-//	}
 }
